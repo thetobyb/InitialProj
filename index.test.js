34d65808cd4df123d1de09c3f97877e36c1ae9b7
Toby.Broadbent <tobybroadbent@aol.com>
2021-11-01 15:45:18 +0000

    slim code

--- index.test.js
+++ index.test.js
@@ -7,24 +7,16 @@ const ron = index.ron
 test('That the lions strength and rest increases rested 82/72 str 55/60', () => {
 
     ron.train()
-    
     expect(ron.str).toBe(60)
-
     expect(ron.rested).toBe(72)
-
     
-
 })
 
 test('That the lions rest decreases by another 10 72/62, and bitestrength goes up 1 35/37', () => {
 
     
     ron.eat()
-    
     expect(ron.rested).toBe(62)
-
-    
-
     expect(ron.biteStr).toBe(37)
 
 })
@@ -32,9 +24,6 @@ test('That the lions rest decreases by another 10 72/62, and bitestrength goes u
 test('That the lions sleep goes to 100', () => {
 
     ron.sleep()
-    
-    
-
     expect(ron.rested).toBe(100)
 
     
