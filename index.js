2ed742295d884f7101af5a955dca73bc86745edb
Toby.Broadbent <tobybroadbent@aol.com>
2021-11-01 15:53:43 +0000

    add Goat class

--- index.js
+++ index.js
@@ -27,10 +27,42 @@ class  Lion{   
             this.rested = 100;
 
          }}
+
+class  Goat{    
+    constructor(name,  leap, leapStr, agility, rested) {       
+        
+        this.name = name;        
+         this.leap = leap;        
+         this.leapStr = leapStr;        
+         this.agility = agility;       
+        this.sneak = false;        
+        this.legs = 4;        
+        this.rested = rested;   
+         }    
+         
+         train()    {       
+              this.leap += 5;        
+              this.leapStr += 1;   
+              this.rested -= 10;
+              
+            }    
+            
+        eat()   {        
+            this.leapStr +=1;
+            this.rested -= 10;
+        }    
+            
+        sleep() {           
+
+            this.rested = 100;
+
+         }}
+
+
          
          
          const ron = new Lion('Ron', 55, 35, 75, 82)
-         
+         const Doe = new Goat('Doe', 70, 33, 40, 60)
          
          module.exports = {  
              
